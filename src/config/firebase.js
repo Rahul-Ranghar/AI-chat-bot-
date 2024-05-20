450c59c4ed1198f865e29935851a0d61d3bb1d10
Rahul Ranghar <90377082+Rahul-Ranghar@users.noreply.github.com>
2024-05-20 08:11:05 -0700

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {collection, getFirestore, doc} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2b7BW0k0J2rN9rYwrlq99CBZf2mvN3Rc",
+  apiKey: "",
   authDomain: "rio-ai-chat-bot.firebaseapp.com",
   projectId: "rio-ai-chat-bot",
   storageBucket: "rio-ai-chat-bot.appspot.com",
   messagingSenderId: "387863839446",
-  appId: "1:387863839446:web:848806a93469307306bf77",
-  measurementId: "G-8EDBP7VP1M"
+  appId: "",
+  measurementId: ""
   };
 
   const app = initializeApp(firebaseConfig);
